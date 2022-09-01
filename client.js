1739830be8589cc985d4ef8746daeac1fd1a92a7
nickholmstyle <nick416holm@protonmail.com>
2022-09-01 04:21:37 +0000

    working with Coreen snake program

--- client.js
+++ client.js
@@ -1,25 +1,62 @@
+
+
 const net = require("net");
 
+// establishes a connection with the game server
 const connect = function () {
-  console.log("Connecting ...");
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: "localhost",
+    port: 50541,
   });
-  conn.on("connect", () => {
-    console.log('Ready to get snekked??');
-    conn.write('Name: NHH')
-  })
-  conn.on("data", () => {
-  console.log("you ded cuz you idled:");
-  })
-  
-  
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    // conn.write("Name: NHH");
+    // conn.write("Move: up");
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 500)     
+  });
+
+  conn.on("data", () => {
+    console.log("you ded cuz you idled");
+  });
+
   return conn;
 };
 
-module.exports = { 
-  connect
-};
+module.exports = {connect};
+
+
+
+// const net = require("net");
+
+// const connect = function () {
+//   console.log("Connecting ...");
+//   const conn = net.createConnection({
+//     host: 'localhost',
+//     port: 50541
+//   });
+//   conn.on("connect", () => {
+//     console.log('Ready to get snekked??');
+//     conn.write('Name: NHH');
+//     conn.write('Move: up');
+//     conn.write('Move: right');
+//     conn.write('Move: down');
+//   })
+//   conn.on("data", () => {
+//   console.log("you ded cuz you idled:");
+//   })
+  
+  
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   return conn;
+// };
+
+// module.exports = { 
+//   connect
+// };
